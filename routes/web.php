9912d8c723b5cfe5732bd5541df9332ea435a70c
´AxelTomas99´ <axeltomas@iesebre.com>
2021-12-01 17:14:53 +0100

    Laravel JetStream Installed

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 });
 Route::get('/videos/{id}', [VideosController::class,'show']);
 
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
