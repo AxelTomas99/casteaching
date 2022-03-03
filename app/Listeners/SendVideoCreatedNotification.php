2dba3d6f201936201a2e4d4bea60880e56eb47dc
´AxelTomas99´ <axeltomas@iesebre.com>
2022-03-03 19:54:30 +0100

    Video 129 Commit avans de pujar a produccio

--- app/Listeners/SendVideoCreatedNotification.php
+++ app/Listeners/SendVideoCreatedNotification.php
@@ -4,11 +4,10 @@ namespace App\Listeners;
 
 use App\Events\VideoCreated;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Notification;
 use Tests\Unit\SendVideoCreatedNotificationTest;
 
-class SendVideoCreatedNotification
+class SendVideoCreatedNotification implements ShouldQueue
 {
     public static function testedBy()
     {
