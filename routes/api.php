017bae452e40d6ddff03ebdc9c16d4dcdb08f7e5
´AxelTomas99´ <axeltomas@iesebre.com>
2022-03-05 13:19:34 +0100

    Video 131 Laravel Sanctum Proporcionant tokens als usuaris amb TDD

--- routes/api.php
+++ routes/api.php
@@ -28,3 +28,6 @@ Route::middleware(['auth:sanctum','verified'])->group(function (){
     Route::delete('/videos/{id}', [VideosApiController::class,'destroy'])->middleware(['can:videos_manage_destroy']);
 
 });
+
+
+Route::post('/sanctum/token', \App\Http\Controllers\SanctumTokenController::class);
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
 });
 Route::get('/videos/{id}', [VideosController::class,'show']);
 
+
 Route::middleware(['auth:sanctum','verified'])->group(function (){
     Route::get('/manage/videos', [ VideosManageController::class,'index'])->middleware(['can:videos_manage_index'])
         ->name('manage.videos');
