ab13e3314735f20d3604d192292b8b8e1a5d6d67
´AxelTomas99´ <axeltomas@iesebre.com>
2022-04-28 17:18:04 +0200

    140 Canvis a la UI per mostrar correctament imatges amb valors null

--- resources/views/layouts/casteaching.blade.php
+++ resources/views/layouts/casteaching.blade.php
@@ -17,7 +17,7 @@
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased h-full">
         <x-jet-banner />
         <div class="min-h-screen bg-gray-100">
             @livewire('navigation-menu')
